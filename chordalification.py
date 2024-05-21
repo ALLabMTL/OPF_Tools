1df77fd166515f919608155faaba1aff20428d09
victorriko <victor.ricaux@gmail.com>
2024-05-21 10:05:28 -0400

    Chordal Addition

---
+++ chordalification.py
@@ -0,0 +1,141 @@
+import networkx as nx
+import matplotlib.pyplot as plt
+from cvxopt import spmatrix, amd, matrix
+import chompack as cp
+from scipy.sparse import csr_matrix
+from scipy.sparse.linalg import splu
+from enum import Flag
+
+class Relaxation(Flag):
+    SDR = 1
+    Chordal_MFI = 2
+    Chordal_AMD = 3
+    Chordal_MD = 4
+    Chordal_MCS_M = 5
+    SOCR = 6
+    TCR = 7
+    STCR = 8
+    Non_Chordal_Relaxations = SDR | SOCR | TCR | STCR
+    Chordal_Relaxations = Chordal_MFI | Chordal_AMD | Chordal_MD | Chordal_MCS_M
+
+def getRelaxation(name):
+    '''Get the relaxation type from the name'''
+    if name == 'SDR':
+        return Relaxation.SDR
+    elif name == 'Chordal_MFI':
+        return Relaxation.Chordal_MFI
+    elif name == 'Chordal_AMD':
+        return Relaxation.Chordal_AMD
+    elif name == 'Chordal_MD':
+        return Relaxation.Chordal_MD
+    elif name == 'Chordal_MCS_M':
+        return Relaxation.Chordal_MCS_M
+    elif name == 'SOCR':
+        return Relaxation.SOCR
+    elif name == 'TCR':
+        return Relaxation.TCR
+    elif name == 'STCR':
+        return Relaxation.STCR
+    else:
+        return None
+
+def AMD_Cholesky(Network):
+    p = amd.order
+    Sparsity_pattern = nx.to_scipy_sparse_array(Network)
+    Sparsity_pattern.setdiag(1)
+    coo = Sparsity_pattern.tocoo()
+    SP_matrix = cp.symbolic(spmatrix(coo.data.tolist(), coo.row.tolist(), coo.col.tolist(), size=Sparsity_pattern.shape),p)
+    SP_filled = SP_matrix.sparsity_pattern(reordered=False)
+    SP_csr = csr_matrix((list(SP_filled.V), (list(SP_filled.I), list(SP_filled.J))), shape=SP_filled.size)
+    SP_csr.setdiag(0)
+    SP_csr.eliminate_zeros()
+    cliques = SP_matrix.cliques(reordered = False)
+    cliques = [tuple(sorted(i)) for i in cliques]
+    return nx.from_scipy_sparse_array(SP_csr), SP_matrix.p, cliques
+
+
+def elimination_game(Network, heuristic = 'MD'):
+    n = Network.number_of_nodes()
+    Gk = Network.copy()
+    Gplus = Network.copy()
+    Ordering = dict()
+    if heuristic == 'MD':
+        for k in range(n):
+            degrees = dict(nx.degree(Gk))
+            vertex_min_degree = min(degrees, key=degrees.get)
+            Ordering[vertex_min_degree] = k
+            missing_edges = missing_edges_for_clique(Gk, vertex_min_degree)
+            Gk.remove_node(vertex_min_degree)
+            if missing_edges is None:
+                continue
+            Gk.add_edges_from(missing_edges)
+            Gplus.add_edges_from(missing_edges)
+        return Gplus, Ordering
+    if heuristic == 'MFI':
+        for k in range(n):
+            number_of_missing_edges_per_node = dict()
+            for node in Gk:
+                missing_edges = missing_edges_for_clique(Gk, node)
+                if missing_edges is None:
+                    number_of_missing_edges_per_node[node] = 0
+                else:
+                    number_of_missing_edges_per_node[node] = len(missing_edges)
+            vertex_min_missing_edges = min(number_of_missing_edges_per_node, key=number_of_missing_edges_per_node.get)
+            Ordering[vertex_min_missing_edges] = k
+            missing_edges = missing_edges_for_clique(Gk, vertex_min_missing_edges)
+            Gk.remove_node(vertex_min_missing_edges)
+            if missing_edges is None:
+                continue
+            Gk.add_edges_from(missing_edges)
+            Gplus.add_edges_from(missing_edges)
+        return Gplus, Ordering
+
+def missing_edges_for_clique(graph, node):
+    """
+    Find the set of edges needed to make the neighborhood of a node a clique.
+
+    Parameters:
+        graph (nx.Graph): Input graph.
+        node: Node whose neighborhood needs to form a clique.
+
+    Returns:
+        set: Set of edges needed to make the neighborhood of the node a clique.
+    """
+    neighborhood = set(graph.neighbors(node))
+    
+    if is_subclique(graph, neighborhood):
+        return None
+    
+    missing_edges = set()
+
+    # Check for missing edges to form a clique
+    for u in neighborhood:
+        for v in neighborhood:
+            if u < v and not graph.has_edge(u, v):
+                missing_edges.add((u, v))
+
+    return missing_edges
+
+def is_subclique(G,nodelist):
+    H = G.subgraph(nodelist)
+    n = len(nodelist)
+    return H.size() == n*(n-1)/2
+
+def getChordalExtension(Network, relaxation):
+    if relaxation in Relaxation.Non_Chordal_Relaxations:
+        return None
+    if relaxation == Relaxation.Chordal_AMD:
+        return AMD_Cholesky(Network)
+    if relaxation == Relaxation.Chordal_MD:
+        chordal_extension, ordering = elimination_game(Network, 'MD')
+        cliques = nx.chordal_graph_cliques(chordal_extension)
+    if relaxation == Relaxation.Chordal_MFI:
+        chordal_extension, ordering = elimination_game(Network, 'MFI')
+        cliques = nx.chordal_graph_cliques(chordal_extension)
+    if relaxation == Relaxation.Chordal_MCS_M:
+        chordal_extension, ordering = nx.complete_to_chordal_graph(Network)
+        cliques = nx.chordal_graph_cliques(chordal_extension)
+    cliques = [tuple(sorted(i)) for i in cliques]
+    return chordal_extension, ordering, cliques
+
+
--- result.py
+++ result.py
@@ -1,3 +1,5 @@
+import networkx as nx
+
 class RunResult:
     '''Object contains relevant information on optimal values obtained from optimization'''
     def __init__(self):
@@ -6,13 +8,27 @@ class RunResult:
         self.p = None
         self.q = None
         self.loss = 0
+        self.network = None
+        self.chordal_extension = None
+        self.ordering = None
+        self.solve_time = 0
+        self.compilation_time = 0
+        self.relaxation = None
+        self.number_of_cliques = 0
+        self.fill_in = 0
+        self.linking_constraints = 0
+        self.mean_size_of_cliques = 0
 
-    def setAll(self, p, q, W, loss):
+    def setAll(self, p, q, W, loss, network, solve_time, compilation_time, relaxation):
         '''Set all parameters including loss'''
         self.W = W
         self.p = p
         self.q = q
         self.loss = loss
+        self.network = network
+        self.solve_time = solve_time
+        self.compilation_time = compilation_time
+        self.relaxation = relaxation
 
     def setFromVars(self, p, q, W):
         '''Set parameters without loss'''
@@ -23,3 +39,23 @@ class RunResult:
     def setLoss(self, loss):
         '''Set optimal losses'''
         self.loss = loss
+
+    def setAll_chordal(self, p, q, W, loss, network, chordal_extension, ordering, solve_time, compilation_time, relaxation, number_of_cliques, fill_in, linking_constraints, mean_size_of_cliques):
+        '''Set all parameters including loss'''
+        self.W = W
+        self.p = p
+        self.q = q
+        self.loss = loss
+        self.network = network
+        self.chordal_extension = chordal_extension
+        self.ordering = ordering
+        self.solve_time = solve_time
+        self.compilation_time = compilation_time
+        self.relaxation = relaxation
+        self.number_of_cliques = number_of_cliques
+        self.fill_in = fill_in
+        self.linking_constraints = linking_constraints
+        self.mean_size_of_cliques = mean_size_of_cliques
+
+    def is_radial(self):
+        return nx.is_tree(self.network)
